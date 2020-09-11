dc039d82d57e9edd7396f630a1bcc73dc3c7c83d
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-09-10 21:37:45 -0300

    Register app

--- service/rancher.py
+++ service/rancher.py
@@ -14,7 +14,7 @@ class RancherService:
         headers = {
             "Authorization": f"Bearer {self._config.rancher_token}"
         }
-        url = f"{self._config.rancher_url}/clusters"
+        url = f"{self._config.rancher_url}/v3/clusters"
         response = requests.get(url, verify=False, headers=headers)
 
         try:
