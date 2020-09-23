42461226cd6c8aa4dc716b2beb2d0396db63b5a8
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-09-23 16:22:07 -0300

    Change name in default application

--- model/enforcement.py
+++ model/enforcement.py
@@ -11,11 +11,7 @@ class Enforcement:
     repo: str
     path: str
     cluster_name: str
-    _name: str
-
-    @property
-    def name(self):
-        return f"{self.cluster_name}-{self._name}"
+    name: str
 
     def render(self) -> V1alpha1ApplicationSource:
         source = V1alpha1ApplicationSource(path=self.path, repo_url=self.repo)
@@ -47,7 +43,7 @@ def make_default_enforcement(cluster_name: str, config: Config) -> Callable[[], 
         repo=config.enforcement_core_repo,
         path=config.enforcement_core_path,
         cluster_name='in-cluster',
-        _name=config.enforcement_name,
+        name=f"{cluster_name}-{config.enforcement_name}",
     )
 
     default_enforcement.add_parameter('spec.destination.name', cluster_name)
