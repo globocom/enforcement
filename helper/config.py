af3dc3cc13f2a8116084247c1263d1c6c09204dd
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-10-03 00:27:39 -0300

    Refactors `helper` package

--- helper/config.py
+++ helper/config.py
@@ -1,7 +1,8 @@
-import os
 import configparser
-from injector import inject
+import os
 from typing import List
+
+from injector import inject
 
 
 class Config:
@@ -13,42 +14,51 @@ class Config:
 
     @property
     def rancher_url(self) -> str:
-        return os.getenv('RANCHER_URL') or self._config.get('rancher', 'url')
+        return self.__get_config_value('RANCHER_URL', 'rancher', 'url')
 
     @property
     def rancher_token(self) -> str:
-        return os.getenv('RANCHER_TOKEN') or self._config.get('rancher', 'token')
+        return self.__get_config_value('RANCHER_TOKEN', 'rancher', 'token')
 
     @property
     def argo_url(self) -> str:
-        return os.getenv('ARGO_URL') or self._config.get('argo', 'url')
+        return self.__get_config_value('ARGO_URL', 'argo', 'url')
 
     @property
     def argo_username(self) -> str:
-        return os.getenv('ARGO_USERNAME') or self._config.get('argo', 'username')
+        return self.__get_config_value('ARGO_USERNAME', 'argo', 'username')
 
     @property
     def argo_password(self) -> str:
-        return os.getenv('ARGO_PASSWORD') or self._config.get('argo', 'password')
+        return self.__get_config_value('ARGO_PASSWORD', 'argo', 'password')
 
     @property
     def enforcement_core_repo(self) -> str:
-        return os.getenv('ENFORCEMENT_CORE_REPO') or self._config.get('enforcement-core', 'repo')
+        return self.__get_config_value('ENFORCEMENT_CORE_REPO', 'enforcement-core', 'repo')
 
     @property
     def enforcement_core_path(self) -> str:
-        return os.getenv('ENFORCEMENT_CORE_PATH') or self._config.get('enforcement-core', 'path')
+        return self.__get_config_value('ENFORCEMENT_CORE_PATH', 'enforcement-core', 'path')
 
     @property
     def enforcement_name(self) -> str:
-        return os.getenv('ENFORCEMENT_NAME') or self._config.get('enforcement-core', 'name')
+        return self.__get_config_value('ENFORCEMENT_NAME', 'enforcement-core', 'name')
 
     @property
     def ignore_clusters(self) -> List[str]:
-
         try:
-            clusters_str: str = os.getenv('IGNORE_CLUSTERS') or self._config.get('rancher', 'ignore_clusters')
-            clusters = list(map(lambda cluster: cluster.strip(), clusters_str.split(",")))
+            clusters_str = self.__get_config_value(
+                'IGNORE_CLUSTERS', 'rancher', 'ignore_clusters'
+            )
+            clusters = [cluster.strip() for cluster in clusters_str.split(',')]
             return clusters
         except configparser.NoOptionError:
             return []
+
+    def __get_config_value(
+            self, environemnt_variable_name: str, config_name: str, config_attribute: str,
+    ) -> str:
+        return (
+            os.getenv(environemnt_variable_name)
+            or self._config.get(config_name, config_attribute)
+        )
