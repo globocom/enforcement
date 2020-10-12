5596a5d208ce8e54ba274ad3425d767524197c56
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-10-12 20:20:24 -0300

    List clusters in base datasource

--- data/datasource/datasource.py
+++ data/datasource/datasource.py
@@ -1,7 +1,8 @@
-from typing import List, Dict, Any
+from typing import List
+from model.cluster import Cluster
 
 
 class ClusterDatasource:
-    def get_clusters(self, params: dict) -> List[Dict[str, Any]]:
+    def get_clusters(self, params: dict) -> List[Cluster]:
         raise Exception('Not implement')
 
