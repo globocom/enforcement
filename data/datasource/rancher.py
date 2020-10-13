7582c86138eeec4093210b2ac7674dba389b7d4b
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-10-13 17:08:48 -0300

    Filter cluster

--- data/datasource/rancher.py
+++ data/datasource/rancher.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Dict
 import requests
 
 from data.datasource.datasource import ClusterDatasource
-from model.entities import RancherSource, Cluster, EnforcementSource
+from model.entities import Cluster, EnforcementSource
 
 
 class RancherDatasource(ClusterDatasource):
@@ -21,20 +21,34 @@ class RancherDatasource(ClusterDatasource):
             url, verify=False, headers=headers, params=filters,
         ) as response:
             response.raise_for_status()
-            return self._build_clusters(response.json()['data'])
-
-    def _build_clusters(self, cluster_map_list: dict) -> List[Cluster]:
-        clusters: List[Cluster] = []
-        for cluster in cluster_map_list:
-            clusters.append(
-                Cluster(
-                    name=cluster['name'],
-                    id=cluster['id'],
-                    token=self._config.rancher_token,
-                    url=f'{self._config.rancher_url}/k8s/clusters/{cluster["id"]}',
-                )
+            return self._filter_and_map_clusters(
+                response.json()['data'], source.rancher.labels, source.rancher.ignore
             )
-        return clusters
+
+    def _filter_and_map_clusters(self, clusters_list: List[Dict], labels: dict, ignore: List[str]) -> List[Cluster]:
+        return list(
+                    map(
+                        self._build_cluster,
+                        filter(
+                            lambda cluster_map: self._filter_cluster(cluster_map, labels, ignore),
+                            clusters_list
+                        )
+                    )
+        )
+
+    def _filter_cluster(self, cluster_map: dict, labels: dict, ignore: List[str]) -> bool:
+        if ignore and cluster_map['name'] in ignore:
+            return False
+
+        return set(labels.items()).issubset(set(cluster_map['labels'].items())) if labels else True
+
+    def _build_cluster(self, cluster_map: dict) -> Cluster:
+        return Cluster(
+            name=cluster_map['name'],
+            id=cluster_map['id'],
+            token=self._config.rancher_token,
+            url=f'{self._config.rancher_url}/k8s/clusters/{cluster_map["id"]}',
+        )
 
 
 
