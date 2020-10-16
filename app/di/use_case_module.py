f26e8b984194c6517232870a205e953f0e4ee6c4
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-10-16 18:16:51 -0300

    create usecases

--- app/di/use_case_module.py
+++ app/di/use_case_module.py
@@ -3,6 +3,8 @@ from injector import Module, provider, singleton
 from app.use_case.register_clusters import RegisterClustersUseCase
 from app.use_case.apply_rules import ApplyRulesUseCase
 from app.use_case.apply_enforcements import ApplyEnforcementsUseCase
+from app.use_case.detect_new_clusters import DetectNewClustersUseCase
+from app.use_case.sync_rules import SyncRulesUseCase
 
 from app.data.repository.cluster import ClusterRepository
 from app.data.repository.enforcement import EnforcementRepository
@@ -29,6 +31,25 @@ class UseCaseModule(Module):
 
     @provider
     @singleton
+    def provider_sync_rules(
+            self, detect_new_clusters: DetectNewClustersUseCase, apply_enforcements: ApplyEnforcementsUseCase,
+            locator: ClusterDataSourceLocator
+    ) -> SyncRulesUseCase:
+        return SyncRulesUseCase(
+            datasource_locator=locator,
+            apply_enforcements_use_case=apply_enforcements,
+            detect_new_clusters_use_case=detect_new_clusters
+        )
+
+    @provider
+    @singleton
     def provider_apply_enforcements(self, enforcement_repository: EnforcementRepository) -> ApplyEnforcementsUseCase:
         return ApplyEnforcementsUseCase(enforcement_repository=enforcement_repository)
+
+    @provider
+    @singleton
+    def provider_detect_new_clusters(self, cluster_repository: ClusterRepository) -> DetectNewClustersUseCase:
+        return DetectNewClustersUseCase(
+            cluster_repository=cluster_repository
+        )
 
