1f4f74d3747403645582b64844a8c130c7aa6ed5
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-10-19 16:01:39 -0300

    Implement SyncRulesUseCase

--- app/domain/use_case/sync_rules.py
+++ app/domain/use_case/sync_rules.py
@@ -4,6 +4,8 @@ from typing import List
 from app.domain.entities import ClusterRule, Cluster
 from app.domain.source_locator import SourceLocator
 from app.domain.repositories import EnforcementRepository, ClusterRepository
+from app.domain.cluster_group import ClusterGroup
+from app.domain.enforcement_installer import EnforcementInstaller
 
 
 @attr.s(auto_attribs=True)
@@ -16,7 +18,23 @@ class SyncRulesUseCase:
         source_repository = self._source_locator.locate(cluster_rule.source)
         source_clusters = source_repository.get_clusters()
 
-        source_cluster_group = self._cluster_group_builder.build(source_clusters)
-        current_cluster_group = self._cluster_group_builder.build(current_clusters)
+        source_cluster_group = ClusterGroup(clusters=source_clusters, cluster_repository=self._cluster_repository)
+        current_cluster_group = ClusterGroup(clusters=current_clusters, cluster_repository=self._cluster_repository)
+
+        deleted_clusters = current_cluster_group - source_cluster_group
+        deleted_clusters.unregister()
+
+        new_clusters = source_cluster_group - current_cluster_group
+        new_clusters.register()
+
+        enforcement_installer = EnforcementInstaller(
+            enforcement_repository=self._enforcement_repository,
+            cluster_group=new_clusters,
+            enforcements=cluster_rule.enforcements
+        )
+
+        enforcement_installer.install()
+        return source_clusters
+
 
 
